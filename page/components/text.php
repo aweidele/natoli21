9a86f4bd4cdeb8191a26134a29dab914d0ad952d
Aaron Weidele <aweidele@gmail.com>
2021-07-01 13:54:19 -0400

    quotes

--- page/components/text.php
+++ page/components/text.php
@@ -1,1 +1,10 @@
-Text
+<?php
+/**
+ * Description: Flexible tagline
+ *
+ * @package WordPress
+ * @subpackage Natoli
+ */
+$content = get_sub_field("content", $currentId);
+?>
+<div class="page__copy"><?= do_shortcode($content) ?></div>
