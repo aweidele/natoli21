4a1e6d1202a6a021f7dc7c89004abbcd04d2e25b
Aaron Weidele <aweidele@gmail.com>
2021-07-01 11:38:54 -0400

    Setting up page columns

--- page/page.php
+++ page/page.php
@@ -8,13 +8,15 @@
 include( TEMPLATEPATH."/page/components/banner.php" );
 ?>
 <div class="page__wrapper">
-  <div class="page__left">
-    <?php include( TEMPLATEPATH."/page/components/tagline.php" ); ?>
-  </div>
-  <div class="page__middle">
-    <?php include( TEMPLATEPATH."/page/components/copy.php" ); ?>
-  </div>
-  <div class="page__right">
-    <?php include( TEMPLATEPATH."/page/components/flexible.php" ); ?>
+  <div class="page__row">
+    <div class="page__left col">
+      <?php include( TEMPLATEPATH."/page/components/tagline.php" ); ?>
+    </div>
+    <div class="page__middle col">
+      <?php include( TEMPLATEPATH."/page/components/copy.php" ); ?>
+    </div>
+    <div class="page__right col">
+      <?php include( TEMPLATEPATH."/page/components/flexible.php" ); ?>
+    </div>
   </div>
 </div>
