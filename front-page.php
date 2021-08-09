97844ba244da23182645cf34f472f366e4456fd1
Aaron Weidele <aweidele@gmail.com>
2021-08-09 09:59:49 -0400

    Setting up front page layout

---
+++ front-page.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Natoli
+ */
+get_header();
+
+$leftDelay = get_field("left_initial_delay", "options");
+$leftTimeout = get_field("left_slide_delay", "options");
+$leftSpeed = get_field("left_transition_speed", "options");
+$rightDelay = get_field("right_initial_delay", "options");
+$rightTimeout = get_field("right_slide_delay", "options");
+$rightSpeed = get_field("right_transition_speed", "options");
+$taglineDelay = get_field("tagline_initial_delay", "options");
+$taglineTimeout = get_field("tagline_slide_delay", "options");
+$taglineSpeed = get_field("tagline_transition_speed", "options");
+?>
+<main class="front-page">
+  <div class="front-page__container">
+    <div class="front-page__container-left">
+      <!-- LEFT SLIDER -->
+      <div class="front-page__slider">
+      <?php
+				while( has_sub_field("left_slides", "options") ) {
+          $image = get_sub_field("image");
+					$imageLandscape = get_sub_field("image_landscape");
+					$focalX = get_sub_field("focal_x");
+					$focalY = get_sub_field("focal_y");
+
+					$landscapeFocalX = get_sub_field("landscape_focal_x");
+					$landscapeFocalY = get_sub_field("landscape_focal_y");
+      ?>
+        <div>
+          <?php if( $imageLandscape ) { ?>
+          <img alt="<?= $image["alt"] ?>" src="<?=$image["url"]?>" class="portrait">
+          <img alt="<?= $imageLandscape["alt"] ?>" src="<?=$imageLandscape["url"]?>" class="landsape">
+          <?php } else { ?>
+          <img alt="<?= $image["alt"] ?>" src="<?=$image["url"]?>">
+          <?php } ?>
+        </div>
+      <?php
+        }
+      ?>
+      </div>
+    </div>
+    <div class="front-page__container-right">
+      Hello.
+    </div>
+  </div>
+</main>
+<?php
+get_footer();
