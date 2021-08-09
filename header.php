0ff789bd0007d68600a244ab2eb5c16819a0fe84
Aaron Weidele <aweidele@gmail.com>
2021-08-09 15:08:39 -0400

    what's wrong with the viewport?

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
   	?></title>
     <meta name="author" content="">
   	<!--  Mobile Viewport Fix -->
-  	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
 
   	<!-- Place favicon.ico and apple-touch-icons in the images folder -->
   	<!-- <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico"> -->
